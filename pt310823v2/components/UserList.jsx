e53512dbc5e229bc2387a46936471fde71d18830
Aluon <paulacanoroman@gmail.com>
2023-08-31 13:56:59 +0200

    feat/ useEffect con llamada API axios
    Agrega un estado utilizando useState para almacenar la lista de usuarios.
    Utiliza el efecto useEffect para realizar una solicitud GET a la URL https://jsonplaceholder.typicode.com/users al montar el componente.

--- pt310823v2/components/UserList.jsx
+++ pt310823v2/components/UserList.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState} from 'react'
+import axios from 'axios';
 
 const UserList = () => {
 
 
     const [datosAPI, setDatosAPI] = useState([])
 
+    useEffect(() => {
+        llamadaAPI()
+    }, [])
+
+    const llamadaAPI = () => {
+        const url = `https://jsonplaceholder.typicode.com/users`;
+        axios.get(url)
+        .then((response)=>console.log(response.data))
+        .catch(error => console.log('Error al obtener los datos:', error));
+    }
+    
 
   return (
     <div>UserList</div>
