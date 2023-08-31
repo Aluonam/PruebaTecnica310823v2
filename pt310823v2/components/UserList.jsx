da598e11a078324cbe5dad862e74df1ad9d9f066
Aluon <paulacanoroman@gmail.com>
2023-08-31 13:53:42 +0200

    feat/ componente y useState

---
+++ pt310823v2/components/UserList.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const UserList = () => {
+
+
+    const [datosAPI, setDatosAPI] = useState([])
+
+
+  return (
+    <div>UserList</div>
+  )
+}
+
+export default UserList
